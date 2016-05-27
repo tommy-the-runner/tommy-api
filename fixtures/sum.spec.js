fd20ac56d83c8d03c78aac0742f9895bbb20bd1e
werckerbot <pleasemailus@wercker.com>
2016-05-27 16:28:43 +0000

    Deploy from Mateusz

--- fixtures/sum.spec.js
+++
@@ -1,8 +0,0 @@
-const expect = require('chai').expect
-const sum = require('subject')
-
-describe('sum', function () {
-  it('should sum two numbers', function () {
-    expect(sum(1, 1)).to.equal(2)
-  })
-})
