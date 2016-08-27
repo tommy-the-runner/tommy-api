69abef1ee8b172c253337d19a04f0de9ad27d5a1
ertrzyiks <mateusz.derks@gmail.com>
2016-08-27 19:48:41 +0200

    Add statsd integration

--- index.js
+++ index.js
@@ -8,8 +8,23 @@ const config = require('config')
 const plugins = require('./src/plugins')(config)
 const server = require('./src/server')(config)
 
+const registerStatsdPlugin = () => {
+    const hapiStatsdConfig = {
+        host: config.get('statsd.host'),
+        port: config.get('statsd.port'),
+        prefix: config.get('statsd.prefix')
+    }
+
+    server.register({ register: require('hapi-statsd'), options: hapiStatsdConfig })
+}
+
 Promise.resolve()
     .then(() => debug('Starting server'))
+    .then(() => {
+        if (config.get('statsd.enabled')) {
+            return registerStatsdPlugin()
+        }
+    })
     .then(() => server.register(plugins.list, plugins.options))
     .then(() => server.start())
     .then(() => debug('Server running at:', server.info.uri))
